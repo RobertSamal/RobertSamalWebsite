966bd2c4f4fdf8e35de5eb4260e4782ff58a871c
Robert Samal <lodekerobert@gmail.com>
2023-04-24 23:13:04 +0300

    This is my main page

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="mystyles/styles.css">
+    <title>This is my first PHP
+    </title>
+</head>
+<body color-"color:white;">
+    <section class="main">
+        <nav>
+            <a href="#" class="logo">
+                <img src="images/menulogo.png" alt="This is the menu" style="width: 100px;">
+            </a>
+
+            <ul>
+                <li><a href="index.php" class="active">Home</a></li>
+                <li><a href="features.php">Features</a></li>
+                <li><a href="about.php">About</a></li>
+                <li><a href="contact.php">Contact Us</a></li>
+            </ul>
+        </nav>
+
+    </section>
+    
+</body>
+</html>
