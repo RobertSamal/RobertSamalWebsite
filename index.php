e1ceaa630d47f8d3cc779839bb841240fa08320f
Robert Samal <lodekerobert@gmail.com>
2023-04-25 21:40:11 +0300

    Added sections in the body eg contact and about us

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
     <section class="main">
         <nav>
             <a href="#" class="logo">
-                <img src="images/menulogo.png" alt="This is the menu" style="width: 100px;">
+                <img src="images/menulogo.png" alt="This is the menu" style="width: 70px;">
             </a>
 
             <ul>
@@ -23,6 +23,82 @@
             </ul>
         </nav>
 
+        <div class="main-part">
+            <h1>My Website</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis molestias labore sunt non quo.</p>
+            <a href="features.php" class="main-btn">Let's Go!</a>
+        </div>
+
+    </section>
+
+    <section class="features">
+        <div class="features-container">
+            <div class="feature-box">
+                <div class="f-img">
+                    <img src="images/computer.jpg" alt="This is a picture of a PC" style="width: 100%; height: 100%"   >
+
+                </div>
+                <div class="f-text">
+                    <h4>Website privacy</h4>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae, debitis facere. Dolore.</p>
+                    <a href="#" class="main-btn">Check</a>
+
+                </div>
+            </div>
+
+            <div class="feature-box">
+                <div class="f-img">
+                    <img src="images/computer.jpg" alt="This is a picture of a PC" style="width: 100%; height: 100%"   >
+
+                </div>
+                <div class="f-text">
+                    <h4>Website privacy</h4>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae, debitis facere. Dolore.</p>
+                    <a href="#" class="main-btn">Check</a>
+
+                </div>
+            </div>
+
+            <div class="feature-box">
+                <div class="f-img">
+                    <img src="images/computer.jpg" alt="This is a picture of a PC" style="width: 100%; height: 100%"   >
+
+                </div>
+                <div class="f-text">
+                    <h4>Website privacy</h4>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae, debitis facere. Dolore.</p>
+                    <a href="#" class="main-btn">Check</a>
+
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <section class="about">
+        <div class="about-img">
+            <img src="images/mypic.jpg" alt="My picture" width=400px>
+        </div>
+        <div class="about-text">
+            <h2>About us</h2>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugit sapiente, ipsa aspernatur soluta, alias ipsam eveniet quaerat maxime, laborum odio recusandae. Quisquam, dolorum.</p>
+            <a href="about.php" class="main-btn">See More</a>
+        </div>
+    </section>
+
+    <section class="contact">
+        <div class=contact-heading>
+            <h1>Contact Me</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque ea laborum perferendis.</p>
+        </div>
+
+        <form action="userinformation.php" method="post">
+            <input type="text" name="user" placeholder="Your Full Name" required="">
+            <input type="email" name="emial" placeholder="Your Email">
+            <textarea name="message" placeholder="Type your message"  cols="30" rows="10"></textarea>
+            <button class="main-btn contact-btn" type="submit">Continue</button>
+
+        </form>
     </section>
     
 </body>
